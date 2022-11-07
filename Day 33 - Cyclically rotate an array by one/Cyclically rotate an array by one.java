dfb3aeb5556115f6fe24b7bfc81421de27efdbe8
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2022-11-07 22:54:04 +0530

    initial commit

---
+++ Day 33 - Cyclically rotate an array by one/Cyclically rotate an array by one.java
@@ -0,0 +1,67 @@
+/*
+Cyclically rotate an array by one
+------------------------------------
+Test Case1:
+Input:
+N = 5
+A[] = {1, 2, 3, 4, 5}
+Output:
+5 1 2 3 4
+
+Test Case2:
+N = 8
+A[] = {9, 8, 7, 6, 4, 2, 1, 3}
+Output:
+3 9 8 7 6 4 2 1
+
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            obj.rotate(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    {
+         int to_remember = arr[n-1];
+
+    for(int i=n-1; i>=1; i--){
+
+        arr[i]=arr[i-1];
+
+    }
+
+    arr[0]=to_remember;
+
+    }
+}
