e7661cf7156c54244d8c07c4f0f00e49b88de7c0
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2022-11-15 21:50:07 +0530

    Create Pattern 6.java

---
+++ Day 41 - Patterns 6 & 7/Pattern 6.java
@@ -0,0 +1,57 @@
+/*
+Patter 6
+---------
+1. You are given the number n.
+2. You've to create a pattern of * and separated it by space as shown in the output format.
+
+Input Format:
+A number n
+
+Output Format:
+*	
+	*	
+		*	
+			*	
+				*	
+					*	
+
+Test Case:
+
+Input:
+Enter the value of n: 5
+Output:
+
+*	
+	*	
+		*	
+			*	
+				*		
+
+ */
+
+Code:
+
+import java.util.*;
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int st = 1;
+    int sp = 0;
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 0; j < sp; j++)
+      {
+        System.out.print("\t");
+      }
+      sp += 1;
+      for (int j = 0; j < 1; j++)
+      {
+        System.out.print("*\t");
+      }
+      System.out.println();
+    }
+
+  }
+}
