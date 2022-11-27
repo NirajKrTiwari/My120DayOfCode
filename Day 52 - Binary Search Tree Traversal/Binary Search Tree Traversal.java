03decee460d5219721c48e24a5a63e5017ea32b8
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2022-11-27 19:35:45 +0530

    initial commit

---
+++ Day 52 - Binary Search Tree Traversal/Binary Search Tree Traversal.java
@@ -0,0 +1,97 @@
+
+/*
+Binary Search Tree Traversal
+-----------------------------
+
+Test Case:
+
+Input:
+Enter the root node: 23
+Enter the number of child node you want to enter: 3
+Enter Value: 12
+Enter Value: 1
+Enter Value: 45
+
+Output:
+Inorder Traversal: 1 12 23 45 
+Preorder Traversal: 23 12 1 45 
+Postorder Traversal: 1 12 45 23 
+
+ */
+import java.util.Scanner;
+public class BST_Deletion {
+    static class Node{
+        int value;
+        Node left,right;
+        public Node(int value)
+        {
+            this.value=value;
+            left=right=null;
+        }
+    }
+    public void insert(Node node,int value) {
+        if (value < node.value) {
+            if (node.left != null) {
+                insert(node.left, value);
+            } else {
+                node.left = new Node(value);
+            }
+        } else if (value > node.value) {
+            if (node.right != null) {
+                insert(node.right, value);
+            } else {
+                node.right = new Node(value);
+            }
+        }
+    }
+        public void InOrder(Node node)
+        {
+            if(node!=null)
+            {
+                InOrder(node.left);
+                System.out.print(node.value+" ");
+                InOrder(node.right);
+            }
+        }
+        public void PreOrder(Node node)
+        {
+            if(node!=null)
+            {
+                System.out.print(node.value+" ");
+                PreOrder(node.left);
+                PreOrder(node.right);
+            }
+        }
+    public void PostOrder(Node node)
+    {
+        if(node!=null)
+        {
+            PostOrder(node.left);
+            PostOrder(node.right);
+            System.out.print(node.value+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        BST_Deletion tree=new BST_Deletion();
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter the root node: ");
+        int root_vale=in.nextInt();
+        Node root=new Node(root_vale);
+        System.out.print("Enter the number of child node you want to enter: ");
+        int n=in.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("Enter Value: ");
+            int data=in.nextInt();
+            tree.insert(root, data);
+        }
+        System.out.print("Inorder Traversal: ");
+        tree.InOrder(root);
+        System.out.print("\nPreorder Traversal: ");
+        tree.PreOrder(root);
+        System.out.print("\nPostorder Traversal: ");
+        tree.PostOrder(root);
+    }
+}
+
