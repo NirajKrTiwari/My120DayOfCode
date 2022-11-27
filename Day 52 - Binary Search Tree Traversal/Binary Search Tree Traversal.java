a20de4d6c337ff3991c84e803c2357f6d51baae5
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2022-11-27 19:37:17 +0530

    Update Binary Search Tree Traversal.java

--- Day 52 - Binary Search Tree Traversal/Binary Search Tree Traversal.java
+++ Day 52 - Binary Search Tree Traversal/Binary Search Tree Traversal.java
@@ -1,23 +1,4 @@
 
-/*
-Binary Search Tree Traversal
------------------------------
-
-Test Case:
-
-Input:
-Enter the root node: 23
-Enter the number of child node you want to enter: 3
-Enter Value: 12
-Enter Value: 1
-Enter Value: 45
-
-Output:
-Inorder Traversal: 1 12 23 45 
-Preorder Traversal: 23 12 1 45 
-Postorder Traversal: 1 12 45 23 
-
- */
 import java.util.Scanner;
 public class BST_Deletion {
     static class Node{
