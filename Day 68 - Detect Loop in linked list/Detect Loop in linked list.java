71fe38347585ff307f6cb0e31a631d76a70db0ec
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2022-12-28 21:24:57 +0530

    initial commit

---
+++ Day 68 - Detect Loop in linked list/Detect Loop in linked list.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            if( x.detectLoop(head) )
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+
+
+class Solution {
+    public static boolean detectLoop(Node head){
+        Node low=head;
+        Node high=head;
+        if(head==null)
+        {
+            return false;
+        }
+        while(high!=null && high.next!=null)
+        {
+            low=low.next;
+            high=high.next.next;
+            if(low==high)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
