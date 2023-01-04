9d9faababc6f15c80f1d46abd6df79372f00ae6d
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-01-04 22:35:35 +0530

    initial commit

---
+++ Day 75 - Intersection of two sorted Linked lists/Intersection of two sorted Linked lists.java
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+class GfG
+{
+    static Scanner sc = new Scanner(System.in);
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        
+        head = tail = new Node(val);
+        
+        size--;
+        
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+        {
+            
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n , m;
+                    
+                    n = sc.nextInt();
+                    m = sc.nextInt();
+                    
+                    Node head1 = inputList(n);
+                    Node head2 = inputList(m);
+                    
+                    Sol obj = new Sol();
+                    
+                    Node result = obj.findIntersection(head1, head2);
+	    
+	                printList(result);
+	                System.out.println();
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Sol
+{
+   public static Node findIntersection(Node head1, Node head2)
+    {
+         Node newNode=new Node(-1);
+         Node temp=newNode;
+         Node curr1=head1,curr2=head2;
+         while(curr1!=null && curr2!=null)
+         {
+             if(curr1.data==curr2.data)
+             {
+                 Node n=new Node(curr1.data);
+                 temp.next=n;
+                 temp=temp.next;
+                 curr1=curr1.next;
+                 curr2=curr2.next;
+             }
+             else if(curr1.data>curr2.data)
+             {
+                 curr2=curr2.next;
+             }
+             else
+             {
+                 curr1=curr1.next;
+             }
+         }
+         return newNode.next;
+    }
+}
---
+++ Day 75 - Intersection of two sorted Linked lists/README.md
@@ -0,0 +1,18 @@
+Intersection of two sorted Linked lists
+----------------------------------------
+Testcase 1:
+
+Input:
+L1 = 1->2->3->4->6
+L2 = 2->4->6->8
+Output: 2 4 6
+Explanation: For the given first two
+linked list, 2, 4 and 6 are the elements
+in the intersection. 
+
+Testcase 2:
+
+Input:
+L1 = 10->20->40->50
+L2 = 15->40
+Output: 40
