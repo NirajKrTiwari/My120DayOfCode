52d094b2fa51aa2cd487403ceb19e8fc4c66d25e
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-01-05 23:16:44 +0530

    initial commit

---
+++ Day 76 - Intersection Point in Y Shapped Linked Lists/Intersection Point in Y Shapped Linked Lists.java
@@ -0,0 +1,184 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+	
+ class LinkedList_Intersection
+{
+    Node head = null;  
+	Node tail = null;
+
+public void addToTheLast(Node node) 
+{
+
+  if (head == null) {
+   head = node;
+   tail = head;
+  } else {
+   //Node temp = head;
+   //while (temp.next != null)
+    //temp = temp.next;
+
+   //temp.next = node;
+   tail.next=node;
+   tail = node;
+  }
+}
+
+  /* Function to print linked list */
+    void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+	 
+ 
+     /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+       
+         
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			int n3 = sc.nextInt();
+			LinkedList_Intersection llist1 = new LinkedList_Intersection();
+		    LinkedList_Intersection llist2 = new LinkedList_Intersection();
+			LinkedList_Intersection llist3 = new LinkedList_Intersection();
+			
+				int a1=sc.nextInt();
+				Node head1= new Node(a1);
+				Node tail1= head1;
+				
+				for (int i = 1; i < n1; i++) 
+				{
+					int a = sc.nextInt(); 
+					tail1.next = (new Node(a));
+					tail1= tail1.next;
+				}
+			
+			
+				int b1=sc.nextInt();
+				Node head2 = new Node(b1);
+				Node tail2 = head2;
+				for (int i = 1; i < n2; i++) 
+				{
+					int b = sc.nextInt();  
+					tail2.next = (new Node(b));
+					tail2= tail2.next;
+				}
+				if(n3>0){
+				int c1=sc.nextInt();
+				Node head3= new Node(c1);
+				tail1.next = head3;
+				tail2.next = head3;
+				Node tail3=head3;
+				for (int i = 1; i < n3; i++) 
+				{
+					int c = sc.nextInt();   
+					tail3.next = (new Node(c));
+					tail3= tail3.next;
+				}
+				}
+				Intersect obj = new Intersect();
+				System.out.println(obj.intersectPoint(head1, head2));
+			t--;			
+         }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}*/
+
+class Intersect
+{
+    static int count(Node head)
+    {
+        Node temp=head;
+        int c=0;
+        while(temp!=null)
+        {
+            c++;
+            temp=temp.next;
+        }
+        return c;
+    }
+    //Function to find intersection point in Y shaped Linked Lists.
+	int intersectPoint(Node head1, Node head2)
+	{
+         int c1=count(head1);
+         int c2=count(head2);
+         int len=0;
+         Node temp1=head1;
+         Node temp2=head2;
+         if(temp1==null || temp2==null)
+            return -1;
+         if(c1>c2)
+         {
+             len=c1-c2;
+         }
+         else
+         {
+             len=c2-c1;
+         }
+         if(c1>c2)
+         {
+             for(int i=0;i<len;i++)
+             {
+                 temp1=temp1.next;
+             }
+         }
+         else
+         {
+            for(int i=0;i<len;i++)
+             {
+                 temp2=temp2.next;
+             }
+         }
+         while(temp1!=temp2)
+         {
+                temp1=temp1.next;
+                temp2=temp2.next;
+         }
+         if(temp1==temp2)
+         {
+             return temp1.data;
+         }
+         return -1;
+	}
+}
+
+
---
+++ Day 76 - Intersection Point in Y Shapped Linked Lists/README.md
@@ -0,0 +1,22 @@
+Intersection Point in Y Shapped Linked Lists
+--------------------------------------------
+Testcase 1:
+
+Input: 
+Linked List 1 = 4->1->common
+Linked List 2 = 5->6->1->common
+common = 8->4->5->NULL
+Output: 8
+Explanation: 
+
+4              5
+|              |
+1              6
+ \             /
+  8   -----  1 
+   |
+   4
+   |
+  5
+  |
+  NULL
