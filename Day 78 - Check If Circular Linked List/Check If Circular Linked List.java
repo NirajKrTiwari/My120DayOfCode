13b8c30fccb4a91ab6900a46907ca24f6eff34fe
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-01-07 21:52:31 +0530

    initial commit

---
+++ Day 78 - Check If Circular Linked List/Check If Circular Linked List.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinklIst_Circular
+{
+	Node head;
+	
+	
+	public static void main(String args[])
+	{
+	    Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int l = sc.nextInt();
+			
+			sc.nextLine().trim().split(" ");
+			String str[]= sc.nextLine().trim().split(" ");
+            Node head = null, tail = null;
+            head = new Node(Integer.parseInt(str[0]));
+            tail = head;
+            for(int i = 1; i < n ; i++)
+            {
+                tail.next = new Node(Integer.parseInt( str[i]));
+                tail = tail.next;
+            }
+                
+			
+			if(l==1 && n>=1)
+			{
+				Node temp = head;
+				while(temp.next != null)
+				{
+					temp = temp.next; 
+				}
+				temp.next = head;
+			}
+			
+			GfG g = new GfG();
+			boolean b = g.isCircular(head);
+			if(b==true)
+			System.out.println(1);
+			else 
+			System.out.println(0);
+		t--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+/* Structure of LinkedList
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+class GfG
+{
+    boolean isCircular(Node head)
+    {
+	    Node slow=head;
+	    Node fast=head.next;
+	    if(slow==null)
+	        return false;
+	    while(slow!=fast)
+	    {
+    	       if(fast==null || fast.next==null)
+    	            return false;
+    	       slow=slow.next;
+    	       fast=fast.next.next;
+	    }
+	    return true;
+    }
+}
---
+++ Day 78 - Check If Circular Linked List/README.md
@@ -0,0 +1,17 @@
+Check If Circular Linked List
+------------------------------
+Given head, the head of a singly linked list, find if the linked list is circular or not. A linked list is called circular if it not NULL terminated and all nodes are connected in the form of a cycle. An empty linked list is considered as circular.
+
+Testcase 1:
+
+Input:
+LinkedList: 1->2->3->4->5
+(the first and last node is connected,
+i.e. 5 --> 1)
+Output: 1
+
+Testcase 2:
+
+Input:
+LinkedList: 2->4->6->7->5->1
+Output: 0
