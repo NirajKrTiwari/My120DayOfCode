6c4df655093b9adcadc515d0e597c79418c679ee
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-01-08 22:02:43 +0530

    initial commit

---
+++ Day 79 - Check if Linked List is Palindrome/Check if Linked List is Palindrome.java
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinkedList_Palindrom
+{
+	 Node head;  
+	 Node tail;
+	
+	/* Function to print linked list */
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+ 
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) 
+	{
+
+		if (head == null) 
+		{
+			head = node;
+			tail = node;
+		} else 
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+    }
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		 
+		while(t>0)
+        {
+			int n = sc.nextInt();
+			//int k = sc.nextInt();
+			Is_LinkedList_Palindrom llist = new Is_LinkedList_Palindrom();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+			    tail.next = new Node(a);
+			    tail = tail.next;
+			}
+			
+			Solution g = new Solution();
+			if(g.isPalindrome(head) == true)
+			    System.out.println(1);
+		    else
+			    System.out.println(0);
+			t--;
+		}
+		
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+/* Structure of class Node is
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+
+class Solution
+{
+    boolean isPalindrome(Node head) 
+    {
+        ArrayList<Integer> arr=new ArrayList<>();
+        Node temp=head;
+        while(temp!=null)
+        {
+            arr.add(temp.data);
+            temp=temp.next;
+        }
+        int i=0;
+        int j=arr.size()-1;
+        while(i<=(j+1)/2)
+        {
+            if(arr.get(i)!=arr.get(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }    
+}
+
---
+++ Day 79 - Check if Linked List is Palindrome/README.md
@@ -0,0 +1,23 @@
+Check if Linked List is Palindrome
+-----------------------------------
+Given a singly linked list of size N of integers. The task is to check if the given linked list is palindrome or not.
+
+Testcase 1:
+
+Input:
+N = 3
+value[] = {1,2,1}
+Output: 1
+Explanation: The given linked list is
+1 2 1 , which is a palindrome and
+Hence, the output is 1.
+
+Testcase 2:
+
+Input:
+N = 4
+value[] = {1,2,3,4}
+Output: 0
+Explanation: The given linked list
+is 1 2 3 4 , which is not a palindrome
+and Hence, the output is 0.
