80d6578228fe4c1f2243077cca6f18e3552dc3f8
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-01-15 22:33:25 +0530

    initial commit

---
+++ Day 86 - Multiply two linked lists/Multiply two linked lists.java
@@ -0,0 +1,89 @@
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        next=null;
+    }
+}
+
+class MultiplyTwoLL{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			Node head=null;
+			Node phead=null;
+			int n=sc.nextInt();
+			while(n-->0){
+				int n1=sc.nextInt();
+				if(head==null)
+				head=new Node(n1);
+			else{
+				addHead(head,n1);
+			}
+			}
+			int m=sc.nextInt();
+			while(m-->0){
+				int n1=sc.nextInt();
+				if(phead==null)
+				phead=new Node(n1);
+			else{
+				addPhead(phead,n1);
+			}
+			}
+		GfG g=new GfG();
+
+System.out.println(g.multiplyTwoLists(head,phead));
+		}
+	}
+    public static void addHead(Node node,int a) {
+          if (node == null)
+            return;
+		if(node.next==null)
+			node.next=new Node(a);
+		else
+			addHead(node.next,a);
+	}
+	public static void addPhead(Node node,int a){
+		 if (node == null)
+            return;
+		if(node.next==null)
+		node.next=new Node(a);
+	else
+		addPhead(node.next,a);
+	}
+	}
+	
+
+/*Node is defined as
+class Node
+{
+    int data; 
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class GfG{
+  /*You are required to complete this method */
+   public long multiplyTwoLists(Node l1,Node l2){
+          long n =1000000007;
+         long n1=0,n2=0;
+         while(l1!=null||l2!=null){
+             if(l1!=null){
+                 n1=(n1*10)%n+l1.data;
+                 l1=l1.next;
+             }
+             if(l2!=null){
+                 n2=(n2*10)%n+l2.data;
+                 l2=l2.next;
+             }
+         }
+         return ((n1%n)*(n2%n))%n;
+   }
+}
---
+++ Day 86 - Multiply two linked lists/README.md
@@ -0,0 +1,21 @@
+Multiply two linked lists
+---------------------------------
+Given elements as nodes of the two linked lists. The task is to multiply these two linked lists, say L1 and L2. 
+
+Input:
+2
+2
+3 2
+1
+2
+3
+1 0 0
+2
+1 0 
+Output:
+64
+1000
+
+Explanation:
+Testcase 1: 32*2 = 64.
+Testcase 2: 100*10 = 1000.
