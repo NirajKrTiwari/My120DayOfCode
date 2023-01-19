7856884dc50e2f7192595045fbead88ec189a92b
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-01-19 21:41:18 +0530

    initial commit

---
+++ Day 89 - Middle of Three/Middle of Three.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String[] S = read.readLine().split(" ");
+            int A=Integer.parseInt(S[0]);
+            int B=Integer.parseInt(S[1]);
+            int C=Integer.parseInt(S[2]);
+            Solution ob = new Solution();
+            System.out.println(ob.middle(A,B,C));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int middle(int A, int B, int C){
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(A);
+        list.add(B);
+        list.add(C);
+        Collections.sort(list);
+        int v=list.get(1);
+        return v;
+    }
+}
---
+++ Day 89 - Middle of Three/README.md
@@ -0,0 +1,23 @@
+Middle of Three
+-----------------
+Given three distinct numbers A, B and C. Find the number with value in middle (Try to do it with minimum comparisons).
+
+TestCase 1:
+
+Input:
+A = 978, B = 518, C = 300
+Output:
+518
+Explanation:
+Since 518>300 and 518<978, so 
+518 is the middle element.
+
+TestCase 2:
+
+Input:
+A = 162, B = 934, C = 200
+Output:
+200
+Exaplanation:
+Since 200>162 && 200<934,
+So, 200 is the middle element.
