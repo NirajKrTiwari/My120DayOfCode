3ecd6d4bdc92c5b79d0dffc866a9ca7220492b56
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-02-11 20:35:38 +0530

    initial commit

---
+++ Day113 - Plus One/Print Anagrams Together.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            ArrayList<Integer> arr = new ArrayList<>();
+            
+            for(int i=0 ; i<N ; i++)
+                arr.add(Integer.parseInt(S[i]));
+
+            Solution ob = new Solution();
+            ArrayList<Integer> res = ob.increment(arr,N);
+            
+            for(int i=0; i<res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static ArrayList<Integer> increment(ArrayList<Integer> arr , int N) {
+       for(int i=N-1;i>=0;i--){
+            if(arr.get(i)==9){
+                arr.set(i,0);
+            }else{
+                arr.set(i,arr.get(i)+1);
+                return arr;
+            }
+        }
+        if(arr.get(0)==0){
+            arr.add(0,1);
+        }
+        return arr;
+        } 
+    }
---
+++ Day113 - Plus One/README.md
@@ -0,0 +1,19 @@
+#Plus One
+----------
+TestCase 1:
+Input: 
+N = 3
+arr[] = {1, 2, 4}
+Output: 
+1 2 5
+Explanation:
+124+1 = 125, and so the Output
+
+TestCase 2:
+Input: 
+N = 3
+arr[] = {9,9,9}
+Output: 
+1 0 0 0
+Explanation:
+999+1 = 1000, and so the output
